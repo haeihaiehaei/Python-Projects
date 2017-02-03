e264347e98c1c0da8f7fea8ee1fe98d969dea76d
Martin Persson <martin.r.persson@ericsson.com>
2017-02-03 10:01:23 +0100

    Moved examlpes to README

--- vmware/op5_vcsa_plugin/check_vcsa.py
+++ vmware/op5_vcsa_plugin/check_vcsa.py
@@ -2,8 +2,6 @@
 """
 Op5 check to get the health of the VCenter Appliance via REST API.
 
-Command line example for OP5: $USER1$/check_vcsa_rest.py --username $ARG1$ --password $ARG2$ --url $HOSTNAME$ --domain $ARG3$ --check $ARG4$
-
 Copyright 2017 Martin Persson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
